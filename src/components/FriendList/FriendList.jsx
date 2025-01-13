83c42af0887f286b4e9c4e71d18d65419cbfbbd7
Roman_Tykhiy <2rstykhiy@gmail.com>
2025-01-13 12:11:28 +0200

    hwreact-1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,27 +1,17 @@
 import clsx from "clsx";
 import css from "./FriendList.module.css";
+import FriendListItem from "./FriendListItem";
+
+
 const FriendList = ({friends}) => {
     return (
 
-        <ul className={css.list}>
-            
-            {friends.map(({avatar, name, isOnline, id}) => (
-                    <li key={id}>
-                     <div className={css.listItem}>
-                        <img src={avatar} alt={name} width="96" className={ css.img} />
-                <p className={css.text}>{name}</p>
-                        <p className={clsx(css.textStatusOn, {
-                           [css.textStatusOf] : isOnline == false 
-                        }) }>{isOnline ? "Online" : "Ofline"}</p>
-              
-                    </div>
-                    </li>
-                   
-                ))}
-                
-                
-                
-            
+        <ul className={css.list}> 
+            {friends.map(item => (
+                <li key={item.id}>
+                    <FriendListItem friend={item} />
+                </li>
+            ))}
        </ul>
    )
    
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import css from "./FriendList.module.css";
+const FriendListItem = ({ friend }) => {
+    return (        
+         <div className={css.listItem}>
+            <img src={friend.avatar} alt={friend.name} width="96" className={ css.img} />
+            <p className={css.text}>{friend.name}</p>
+            <p className={clsx(css.textStatusOn, {
+            [css.textStatusOf] : friend.isOnline == false 
+            }) }>{friend.isOnline ? "Online" : "Ofline"}</p>  
+            </div>
+            )   
+   
+}
+
+export default FriendListItem;
